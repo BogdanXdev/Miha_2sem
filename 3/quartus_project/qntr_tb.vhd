342208a0649def99f403c3c04c52cf439cf6df8e
Bogdan Zhukovsky <bogdans.zukovskis@gmail.com>
2021-05-12 02:02:36 +0300

    log2c

--- 3/quartus_project/qntr_tb.vhd
+++ 3/quartus_project/qntr_tb.vhd
@@ -1,33 +1,34 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.util_pckg.all;
 
 entity qntr_tb is
     generic (
-        clks_amount : natural := 41
+        clks_amount : natural
     );
     port (
         clk_in : in std_logic;
         rst_in : in std_logic;
 
-        en_out : out std_logic;
+        en_out : out std_logic
     );
 end entity;
 
 architecture behav of qntr_tb is
 
-    signal counter : unsigned (log2c - 1 downto 0);
-
+    -- signal counter : unsigned ((log2c(clks_amount) - 1) downto 0);
+    signal counter : std_logic_vector ((log2c(clks_amount) - 1) downto 0);
 begin
 
 -- counter reg, nslogic and output logic
     process (clk_in, rst_in)
     begin
-        if rst = '1' then
+        if rst_in = '1' then
             counter <= (others => '0');
-        elsif rising_edge(clk) then
-            counter <= counter + 1;
-            if counter = 41 then
+        elsif rising_edge(clk_in) then
+            counter <= std_logic_vector (unsigned (counter) + 1);
+            if counter = x"29" then
                 counter <= (others => '0');
                 en_out <= '1';
             end if;
