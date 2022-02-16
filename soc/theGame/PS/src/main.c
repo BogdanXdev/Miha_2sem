0b61e741755bdba274d4abcd154ddefc2e5df356
Bogdan Zhukovsky <bogdans.zukovskis@gmail.com>
2022-02-17 01:25:16 +0200

    game

---
+++ soc/theGame/PS/src/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+typedef struct {
+    int x;
+    int y;
+    int side;
+    char* color;
+} primitive;
+
+typedef struct {
+    int primitive_x;
+    int primitive_y;
+    char * color;
+} object;
+
+void main()
+{
+
+// сконструировать конкретные объекты структур
+// интеррапт с клавиатуры меняет координаты в непрерывном режиме - одно нажатие = сдвиг
+// информация об объектах преобразуется в поток данных 
+
+
+
+}
