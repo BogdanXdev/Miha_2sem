2516833f3b478f3613e77a5164c9617734caad69
Bogdans Žukovskis | UAVFACTORY <bzuko01@uavfactory.local>
2022-02-18 17:51:32 +0200

    hps settings added and main.c updated

--- soc/theGame/PS/src/main.c
+++ soc/theGame/PS/src/main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
+#include <stdint.h>
 
 typedef struct {
-    int x;
-    int y;
-    int side;
-    char* color;
+    uint16_t x;
+    uint16_t y;
+    uint16_t side;
 } primitive;
 
-typedef struct {
-    int primitive_x;
-    int primitive_y;
-    char * color;
-} object;
+primitive object_0[3]= {{0,0,10},{0,10,10},{0,20,10}};
+primitive object_1[1]= {{50,50,10}};
+primitive object_2[3]= {{200,0,10},{200,10,10},{200,20,10}};
+
+primitive* frame[]= {object_0, object_1, object_2}; 
+
+void object_movement ();
+void collission_detec ();
 
 void main()
 {
 
-// сконструировать конкретные объекты структур
-// интеррапт с клавиатуры меняет координаты в непрерывном режиме - одно нажатие = сдвиг
-// информация об объектах преобразуется в поток данных 
 
 
 
