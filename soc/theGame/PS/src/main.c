ca115074684835bd17d0e777e7cd3f7795f29925
Bogdans Žukovskis | UAVFACTORY <bzuko01@uavfactory.local>
2022-05-23 15:05:41 +0300

    update.c

--- soc/theGame/PS/src/main.c
+++ soc/theGame/PS/src/main.c
@@ -2,26 +2,28 @@
 #include <stdint.h>
 #include <mechanics.h>
 #include "cma_api.h"
-#include <fcntl.h>			// O_RDWR, O_SYNC
+#include <fcntl.h> // O_RDWR, O_SYNC
 
-#define _I(fmt, args...)	printf(fmt "\n", ##args)
-#define _E(fmt, args...)	printf("ERROR: " fmt "\n", ##args)
+#define _I(fmt, args...) printf(fmt "\n", ##args)
+#define _E(fmt, args...) printf("ERROR: " fmt "\n", ##args)
 
-#define ARRAY_LENGTH		64
-#define DESC_OFFSET			0x20
-#define CSR_OFFSET			0x00
-#define ACP_WINDOW			0x80000000
-
+#define VIDEO_ARRAY_LENGTH 640 * 512
+#define DESC_OFFSET 0x20
+#define CSR_OFFSET 0x00
+#define ACP_WINDOW 0x80000000
+#define READ_REG 0x00
+#define LENGTH_REG 0x08
+#define CONTROL_REG 0x0C
 void display_buffer(uint32_t *buffer, int length)
 {
-	for(int i=0; i<length; i++)
-	{
-		printf("%.6u ", buffer[i]);
+    for (int i = 0; i < length; i++)
+    {
+        printf("%.6u ", buffer[i]);
 
-		// new line after each 8th element
-		if(i % 8 == 7)
-			putchar('\n');
-	}
+        // new line after each 8th element
+        if (i % 8 == 7)
+            putchar('\n');
+    }
 }
 
 uint32_t pixel_nmbr = 0;
@@ -38,7 +40,6 @@ void line_cnt()
 
 uint8_t pixel = 0;
 
-
 /* utilities */
 #define _I(fmt, args...) printf(fmt "\n", ##args)
 #define _E(fmt, args...) printf("ERROR: " fmt "\n", ##args)
@@ -54,25 +55,30 @@ void main()
     _I("Compiled at %s %s", __DATE__, __TIME__);
 
     _I("initializing CMA APIs");
-	if (cma_init() != 0)
-	{
-		_E("failed to initialize CMA");
-		return 1;
-	}
+    if (cma_init() != 0)
+    {
+        _E("failed to initialize CMA");
+        return 1;
+    }
 
     _I("allocating contiguous memory");
-	uint32_t *buffer_src = (uint32_t*)cma_alloc_noncached(ARRAY_LENGTH*sizeof(uint32_t));
-	uint32_t *buffer_dst = (uint32_t*)cma_alloc_noncached(ARRAY_LENGTH*sizeof(uint32_t));
+    uint32_t *buffer_src = (uint32_t *)cma_alloc_noncached(VIDEO_ARRAY_LENGTH * sizeof(uint32_t));
+    //uint32_t *buffer_dst = (uint32_t *)cma_alloc_noncached(VIDEO_ARRAY_LENGTH * sizeof(uint32_t));
 
+    // TO DO: write a function for filling the read register with video data
+    /* TO DO: describe the concept of video data 
+     for example - sending to FPGA part coord-s of the centers of the primitives(primitives are
+     unified pero...) => the video stream could become a sequence of the x-s and y-s for every px
+     on every row
+       */  
     _I("filling memory (src array)");
-	for (uint32_t i=0; i<ARRAY_LENGTH; i++)
-	buffer_src[i] = i;
+    for (uint32_t i = 0; i < VIDEO_ARRAY_LENGTH; i++)
+        buffer_src[i] = i;
 
     _I("displaying memory contents (src)");
-	display_buffer(buffer_src, ARRAY_LENGTH);
-	_I("displaying memory contents (dst)");
-	display_buffer(buffer_dst, ARRAY_LENGTH);
-
+    display_buffer(buffer_src, VIDEO_ARRAY_LENGTH);
+    // _I("displaying memory contents (dst)");
+    // display_buffer(buffer_dst, VIDEO_ARRAY_LENGTH);
 
     int fd;                            /* file descriptor */
     volatile unsigned char *mem_lwh2f; /* memory pointer for LW HPS2FPGA bridge */
@@ -92,21 +98,36 @@ void main()
         return -1;
     }
 
-    _I("Mapping physical address - HPS2FPGA");
-    mem_h2f = mmap(0, HPS2FPGA_SPAN, PROT_READ | PROT_WRITE, MAP_SHARED, fd, HPS2FPGA_BASE);
-    if (mem_h2f == NULL)
-    {
-        _E("Failed to map \"HPS2FPGA\" bridge");
-        return -1;
-    }
+    // _I("Mapping physical address - HPS2FPGA");
+    // mem_h2f = mmap(0, HPS2FPGA_SPAN, PROT_READ | PROT_WRITE, MAP_SHARED, fd, HPS2FPGA_BASE);
+    // if (mem_h2f == NULL)
+    // {
+    //     _E("Failed to map \"HPS2FPGA\" bridge");
+    //     return -1;
+    // }
+
+    _I("running FPGA memcpy:");
+
+    _I("filling descriptor...");
+
+    *((volatile uint32_t *)(mem_lwh2f + DESC_OFFSET + READ_REG)) = ACP_WINDOW + cma_get_phy_addr(buffer_src); ///< read address
+    //*((volatile uint32_t *)(mem_lwh2f + DESC_OFFSET + 0x04)) = ACP_WINDOW + cma_get_phy_addr(buffer_dst); ///< write address
+    *((volatile uint32_t *)(mem_lwh2f + DESC_OFFSET + LENGTH_REG)) = VIDEO_ARRAY_LENGTH * sizeof(uint32_t);           ///< length
+    *((volatile uint32_t *)(mem_lwh2f + DESC_OFFSET + CONTROL_REG)) = 0x80000000;                                ///< control->go
+
+    /// ------ DMA transaction executes here -----
+    _I("DMA transaction executes...");
+
+	/// Waiting for DMA transaction to complete:
+
 
     while (1)
     {
         pads_move();
         ball_move();
 
-    // TO DO: some delay tuning for getting particular FPS how to connect it with a clock and for 
-    //sleep(500); 
+        // TO DO: some delay tuning for getting particular FPS how to connect it with a clock and for
+        // sleep(500);
 
         for (i = 0; i < 512; i++)
         {
@@ -114,9 +135,12 @@ void main()
             for (j = 0; j < 640; j++)
             {
                 //  TO DO: some delay tuning for getting particular FPS
-                if (pixel_compare(j, i, ball, 1) || pixel_compare(j, i, pad_0, 3) || pixel_compare (j, i, pad_1, 3)) {
+                if (pixel_compare(j, i, ball, 1) || pixel_compare(j, i, pad_0, 3) || pixel_compare(j, i, pad_1, 3))
+                {
                     *mem_h2f = 255; // ??? dereferencing the void pointer ??? legal ???
-                } else {
+                }
+                else
+                {
                     *mem_h2f = 0;
                 }
             }
